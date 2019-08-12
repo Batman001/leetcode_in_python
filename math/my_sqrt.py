157a8f83c6921a342a236fe1d857460ae804b856
Batman007 <sun_thinker@163.com>
2019-08-12 11:34:55 +0800

    leetcode 69 平方根的二分求法

--- math/my_sqrt.py
+++ math/my_sqrt.py
@@ -15,5 +15,27 @@ def my_sqrt(num):
     return int(x)
 
 
+def my_sqrt_(x):
+    """
+    二分法求解num的平方根
+    :param x: 待开方数
+    :return: 开方后的结果
+    """
+    ans = 0
+    if x <= 1:
+        return x
+    left, right = 1, x
+    while left <= right:
+        mid = left + ((right - left) >> 1)
+
+        if mid <= x / mid:
+            left = mid + 1
+            ans = mid
+        else:
+            right = mid - 1
+    return ans
+
+
 if __name__ == "__main__":
     print(my_sqrt(256))
+    print(my_sqrt_(2568))
