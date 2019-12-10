a7e1e37d1cf5a2ec372fad06ff97309cf6f242e4
sunc <suncokay@gmail.com>
2019-12-10 13:38:04 +0800

    字符串匹配KMP算法以及暴力破解算法 代码添加

--- kmp/kmp.py
+++ kmp/kmp.py
@@ -2,20 +2,20 @@
 import numpy as np
 
 
-def get_next_array(modestr):
+def get_next_array(mode_str):
     """
     计算得到模式字符串的next数组
-    :param modestr:
-    :return:
+    :param mode_str:模式串
+    :return: 模式字符串的next数组
     """
-    next_array = np.zeros(shape=(len(modestr),), dtype='int32')
+    next_array = np.zeros(shape=(len(mode_str),), dtype='int32')
     next_array[0] = -1
     i = 0
     j = -1
 
-    # while not finish whole modestr
-    while i < len(modestr) - 1:
-        if j == -1 or modestr[i] == modestr[j]:
+    # while not finish whole mode_str
+    while i < len(mode_str) - 1:
+        if j == -1 or mode_str[i] == mode_str[j]:
             # if first element of substring or match, check next
             i += 1
             j += 1
@@ -26,24 +26,24 @@ def get_next_array(modestr):
     return next_array
 
 
-def kmp_algorithm(mainStr, modeStr):
+def kmp_algorithm(main_str, mode_str):
     """
     KMP字符串匹配算法实现
-    :param mainStr:  主字符串
-    :param modeStr:  模式字符串
+    :param main_str:  主字符串
+    :param mode_str:  模式字符串
     :return: 匹配成功的主字符串的索引，否则返回-1
     """
-    next_array = get_next_array(modeStr)
+    next_array = get_next_array(mode_str)
     i, j = 0, 0
     # while not finish loop
-    while i < len(mainStr) and j < len(modeStr):
+    while i < len(main_str) and j < len(mode_str):
         # if first of modeStr or match, check next
-        if j == -1 or mainStr[i] == modeStr[j]:
+        if j == -1 or main_str[i] == mode_str[j]:
             i += 1
             j += 1
         else:
             j = next_array[j]
-    if j == len(modeStr):
+    if j == len(mode_str):
         return i - j
     else:
         return -1
@@ -51,4 +51,6 @@ def kmp_algorithm(mainStr, modeStr):
 
 if __name__ == "__main__":
 
-    print(kmp_algorithm("hello World", "or"))
+    # print(kmp_algorithm("hello World", "or"))
+
+    print(kmp_algorithm("abbaabbaaba", "abbaaba"))
---
+++ kmp/violent_match.py
@@ -0,0 +1,35 @@
+# -*- coding:utf-8 -*-
+
+
+def violent_match(main_str, mode_str):
+    """
+    暴力匹配模式串与主串
+    :param main_str: 主字符串
+    :param mode_str: 模式字符串
+    :return: 匹配成功的主字符串的索引，否则返回-1
+    """
+    # 主串开始的位置
+    i = 0
+    # 模式字符串开始的位置
+    j = 0
+
+    while i < len(main_str) and j < len(mode_str):
+        if main_str[i] == mode_str[j]:
+            i += 1
+            j += 1
+        else:
+            i = i - j + 1
+            j = 0
+
+    # 如果模式字符串匹配完成 即是模式字符串匹配到最后
+    # 由于最后一个字符也需要匹配 因此最后会有 i+=1 j+=1
+    if j == len(mode_str):
+        return i - j
+    else:
+        return -1
+
+
+if  __name__ == "__main__":
+    print(violent_match("Hello world", "or"))
+
+
