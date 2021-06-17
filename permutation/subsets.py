62340974e7cb59e3938bdaa8c737d2c3c4a2720f
sunchao21 <sunchao21@58.com>
2021-06-17 22:06:02 +0800

    leetcode 代码更新

--- permutation/subsets.py
+++ permutation/subsets.py
@@ -1,41 +1,22 @@
 # -*- coding:utf-8 -*-
 
-
-class Solution:
-    # @param S, a list of integer
-    # @return a list of lists of integer
-    def dfs(self, start, S, result, father_subsets):
-        result.append(father_subsets)
-        for i in range(start, len(S)):
-            self.dfs(i+1, S, result, father_subsets+[S[i]])
-
-    def subsets(self, S):
-        # none case
-        if S is None:
-            return []
-        # deep first search
-        result = []
-        self.dfs(0, sorted(S), result, [])
-        return result
+def subsets(nums):
+    res = []
+    temp = []
+    dfs(res, temp, nums, 0)
+    return res
 
 
-class Solve:
-    def dfs(self, start, S, result, subset):
-        result.append(subset)
-        for i in range(start, len(S)):
-            self.dfs(i+1, S, result, subset + [S[i]])
-
-    def subset(self, S):
-        if S is None:
-            return []
-        result = []
-        self.dfs(0, S, result, [])
-        return result
+def dfs(res, temp, nums, index):
+    res.append(temp[:])
+    for i in range(index, len(nums)):
+        temp.append(nums[i])
+        dfs(res, temp, nums, i+1)
+        temp.pop(-1)
 
 
 if __name__ == "__main__":
-    s = Solution()
-    result = s.subsets([1, 2, 3, 4])
+    result = subsets([1, 2, 3, 4])
     print(result)
     print(len(result))
 
