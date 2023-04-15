390d45cd563314f3bf419e4e7ffa89d365b4750d
Batman007 <sun_thinker@163.com>
2023-04-15 11:09:31 +0800

    添加快排思想查找数组中第k大元素的方法

--- sort/TopK.py
+++ sort/TopK.py
@@ -1,81 +1,92 @@
 # -*- coding:utf-8 -*-
+# leetcode215. 数组中的第K个最大元素 https://leetcode.cn/problems/kth-largest-element-in-an-array/
 
-# 维护一个k大的小根堆
-import random
+def build_heap(array):
 
-
-def build_heap(array, k):
-    array = array[:k]
-    for i in range(int(k/2)-1, -1, -1):
+    for i in range(len(array)//2-1, -1, -1):
         minify(array, i)
     return array
 
 
 def minify(array, i):
-    left = left_index(i)
-    right = right_index(i)
-    length = len(array)
-    if left < length and array[left] < array[i]:
-        smallest = left
-    else:
-        smallest = i
-    if right < length and array[right] < array[smallest]:
-        smallest = right
+    left, right = i * 2 + 1, i * 2 + 2
+    length, min_index = len(array), i
 
-    if smallest == i or smallest >= length:
-        return
-    array[i], array[smallest] = array[smallest], array[i]
-    minify(array, smallest)
+    if left < length and array[left] < array[min_index]:
+        min_index = left
+
+    if right < length and array[right] < array[min_index]:
+        min_index = right
+
+    if min_index != i:
+        array[i], array[min_index] = array[min_index], array[i]
+        minify(array, min_index)
 
 
 def heap_sort(heap):
     result = []
     for i in range(len(heap)):
         heap_size = len(heap)
-        heap[0], heap[heap_size - 1] = heap[heap_size -1], heap[0]
-        result.append(heap.pop())
+        heap[0], heap[heap_size - 1] = heap[heap_size-1], heap[0]
+        # 此时最小值已经交换至堆尾
+        result.append(heap.pop(-1))
         minify(heap, 0)
     return result
 
 
-def left_index(i):
-    return 2*i+1
-
-
-def right_index(i):
-    return 2*i+2
-
-
 def get_top_k(array, k):
-    heap = build_heap(array, k)
+    select_array = array[:k]
+    heap = build_heap(select_array)
     for i in range(k, len(array)):
-        if array[i] <= heap[0]:
-            continue
-        else:
+        if array[i] > heap[0]:
             heap[0] = array[i]
             minify(heap, 0)
-    return heap
+    sorted_heap = heap_sort(heap)
+    print("前k大的数组为:", sorted_heap)
+    print("第k大的元素为:", sorted_heap[0])
+    return sorted_heap[0]
 
 
-def random_int_list(start, stop, length):
-    start, stop = (int(start), int(stop)) if start <= stop else (int(stop), int(start))
-    length = int(abs(length)) if length else 0
-    random_list = []
-    for i in range(length):
-        random_list.append(random.randint(start, stop))
-    return random_list
+def find_k_largest_num(nums, k):
+    """
+    使用快排的思想找到数组中第k大元素
+    :param nums: 数组
+    :param k: 第k大
+    :return: 数值
+    """
+    target = len(nums) - k
+    left, right = 0, len(nums)-1
+
+    while True:
+        partition_index = partitions(nums, left, right)
+        if partition_index == target:
+            return nums[partition_index]
+        elif partition_index > target:
+            right -= partition_index - 1
+        else:
+            left = partition_index + 1
 
 
-def main():
-    array = random_int_list(-1000, 1000, 40)
-    print(array)
-    heap = get_top_k(array, 9)
-    result = heap_sort(heap)
-    # 由于result中值为从小到大排列，然后对数字进行整理后输出
-    result = result[::-1]
-    print(result)
+def partitions(nums, left, right):
+    pivot = nums[left]
+    while left < right:
+
+        while left < right and nums[right] >= pivot:
+            right -= 1
+        nums[left] = nums[right]
+
+        while left < right and nums[left] <= pivot:
+            left += 1
+        nums[right] = nums[left]
+
+    nums[left] = pivot
+    return left
 
 
 if __name__ == "__main__":
-    main()
+    array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 34, 25, 564, 683, 26, 675]
+    array_ = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 34, 25, 564, 683, 26, 675]
+    get_top_k(array, 9)
+
+    print('快排思想中，第9大的元素为: ', find_k_largest_num(array_, 9))
 
--- sort/TopKTest.py
+++ sort/TopKTest.py
@@ -10,7 +10,7 @@ def build_heap(array,k):
     :return: 
     """
     array = array[:k]
-    for i in range(int(k/2),-1,-1):
+    for i in range(k//2-1, -1, -1):
         minify(array, i)
     return array
 
@@ -35,8 +35,9 @@ def heap_sort_output(heap):
     result = []
     for i in range(len(heap)):
         heap_size = len(heap)
-        heap[0], heap[heap_size-1] = heap[heap_size -1], heap[0]
-        result.append(heap.pop())  #此时由于最小值交换到堆尾，所以直接pop方法得到最小值
+        heap[0], heap[heap_size-1] = heap[heap_size-1], heap[0]
+        # 此时由于最小值交换到堆尾，所以直接pop方法得到最小值, pop(0)返回第一个值,【pop(-1)等同于pop()返回最后一个值】
+        result.append(heap.pop(-1))
         minify(heap, 0)
     return result
 
@@ -61,15 +62,16 @@ def random_int_list(start, stop, length):
 def main(array, k):
     length = len(array)
     heap = build_heap(array, k)
-    for i in range(k,length):
-        if(array[i]<=heap[0]):
+    for i in range(k, length):
+        if array[i] <= heap[0]:
             continue
         else:
             heap[0] = array[i]
             minify(heap, 0)
     result = heap_sort_output(heap)
     result = result[::-1]
-    print(result)
+    print("前k大的数组为:", result)
+    print("第k大的元素为:", result[-1])
 
 
 if __name__ == "__main__":
